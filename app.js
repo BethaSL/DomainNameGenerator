4b5d7685c1447805b8573ddbf6bbf66d67feb836
Bethania Sanchez <bethasanchez3@gmail.com>
2021-10-31 15:03:48 -0400

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Any string");
+let pronoun = ['the','our'];
+let adj = ['great', 'big'];
+let noun = ['jogger','racoon'];
+let ext = ['.com', '.net', '.us', '.io']
+let domain = [];
+
+for (i=0; i<pronoun.length; i++){
+    for (j=0; j<adj.length; j++){
+        for (k=0; k<noun.length; k++){
+            for (h=0; h<ext.length; h++){
+                domain= pronoun[i] + adj[j] + noun[k] + ext[h];
+                console.log(domain)
+            }
+        }
+    }  
+}
+
